e33ed5c5f1195b5d23e9e4107db7c7cd1e2f669f
hamc0d3r <hamzehmahmood966@gmail.com>
2026-05-15 23:09:33 +0300

    Edits on Payment Domain

--- src/MazadZone.Domain/Payments/Enums/PaymentStatus.cs
+++ src/MazadZone.Domain/Payments/Enums/PaymentStatus.cs
@@ -4,7 +4,8 @@ public enum PaymentStatus
 {
     Pending = 1,
     Authorized = 2, // Funds are held, not captured
-    Completed = 3,  // Funds are successfully captured
-    Failed = 4,
-    Refunded = 5
+    AuthorizedAmountCaptured = 3,   // Funds are successfully captured
+    Completed = 4,  // Funds are successfully captured
+    Failed = 5,
+    Refunded = 6
 }
